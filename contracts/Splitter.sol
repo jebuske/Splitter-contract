9524bf7eb6a7ee13d22f99d63d15959a7e23ef40
Hannes Hardeman <Hannes@Hanness-MacBook-Pro.local>
2017-12-06 17:08:16 +0100

    fixed log, check for constructor not missing values

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -10,7 +10,7 @@ contract Splitter {
   mapping(address => uint) public balances;
 
   
-  event LogSplit(uint value, uint balanceRecipient1, uint balanceRecipient2);
+  event LogSplit(uint value, address from, address recipient1, address recipient2);
   event LogWithdrawal(uint value, address indexed sender);
 
   
@@ -24,22 +24,27 @@ contract Splitter {
   function Splitter(address _recipient1, address _recipient2) {
     // constructor
     owner = msg.sender;
+    assert(_recipient1 != address(0));
+    assert(_recipient2 != address(0));
     recipient1 = _recipient1;
     recipient2 = _recipient2;
-   
   }
+
     //split the amount between two recipients
   function split() payable public onlyByOwner returns(bool) {
     require(msg.value>1);
   balances[recipient1] += msg.value/2;
   balances[recipient2] += msg.value/2;
   uint remainder = msg.value%2;
-  if (remainder>0) balances[msg.sender]+= remainder;
-  LogSplit(msg.value, balances[recipient1], balances[recipient2]);
+  if (remainder>0) {
+    balances[msg.sender]+= remainder;
+    }
+
+  LogSplit(msg.value, msg.sender, recipient1, recipient2);
   return true;
   }
 
-  function withdraw () payable returns (bool) {
+  function withdraw () returns (bool) {
     require(balances[msg.sender]>0);
     uint amount = balances[msg.sender];
     balances[msg.sender] = 0;
